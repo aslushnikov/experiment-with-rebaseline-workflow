97e580d4d0a43f19eb16faa4fa97a16e697d6a6c
Andrey Lushnikov <lushnikov@chromium.org>
2022-03-29 00:33:57 -0700

    extract in a script

---
+++ rebaseline.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+set +x
+
+trap "cd $(pwd -P)" EXIT
+cd "$(dirname "$0")"
+SCRIPT_PATH=$(pwd -P)
+
+COMMIT_PATH=""
+COMMIT_MESSAGE=""
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --path)
+      COMMIT_PATH="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --message)
+      COMMIT_MESSAGE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+  esac
+done
+
+if [[ -z "${COMMIT_MESSAGE}" ]]; then
+  echo "ERROR: --message must be passed"
+  exit 1
+fi
+
+if [[ -z "${COMMIT_PATH}" ]]; then
+  echo "ERROR: --path must be passed"
+  exit 1
+fi
+
+echo "MESSAGE: ${COMMIT_MESSAGE}"
+echo "PATH: ${COMMIT_PATH}"
+
+if [[ -z "$(git status --porcelain $COMMIT_PATH)" ]]; then
+  echo "Nothing to commit"
+  exit 0
+fi
+
+if [[ -z "${GITHUB_HEAD_REF}" ]]; then
+  echo "FYI: this only updates commits in the Pull Request branch"
+  exit 0
+fi
+
+git config --global user.name 'Playwright Rebaseline Bot'
+git config --global user.email 'playwright-rebaseline-bot@users.noreply.github.com'
+git fetch
+git checkout "${GITHUB_HEAD_REF}"
+git add "${COMMIT_PATH}"
+git commit -m "${COMMIT_MESSAGE}"
+git push
