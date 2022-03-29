b42a23b5e9d2da07c44789337a35634c2845f8fe
Andrey Lushnikov <lushnikov@chromium.org>
2022-03-28 22:54:25 -0700

    chore: rebaseline workflow

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect, Page } from '@playwright/test';
 
 test('should allow me to add todo items', async ({ page }) => {
-  await page.goto('https://playwright.dev');
+  await page.goto('https://example.com');
   await expect(page).toHaveScreenshot();
 });
 
